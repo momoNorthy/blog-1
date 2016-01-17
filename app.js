7f1e5214b239b551325cf7372993ac7dd88d9041
sabrinaluo <luohaojing@gmail.com>
2016-01-17 18:45:11 +0800

    test auto update weibo

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const cfg = require('./.travis/config.json');
 fs.readFile('./public/atom.xml', 'utf8', function (err, data) {
   xml2js.parseString(data, function (err, data) {
     const post = data.feed.entry[0];
-    const title = '： 《' + post.title[0] + '》 ';
-    const link = '（戳这里戳这里>>> post.link[0]["$"].href'+' <<<）';
+    const title = ' 《' + post.title[0] + '》 ';
+    const link = '（>>> ' + title + post.link[0]['$'].href + ' <<<）';
     const prefix = [
       '集美貌与智慧于一身的@HiiTea 刚刚更新了博客(๑•̀ㅂ•́)و✧',
       '勤劳又美丽的@HiiTea 又更新博客啦ヽ(✿ﾟ▽ﾟ)ノ',
@@ -21,9 +21,9 @@ fs.readFile('./public/atom.xml', 'utf8', function (err, data) {
       '走过路过千万别错过！'
     ];
 
-    const pre = prefix[(Math.random()*prefix.length)|0];
-    const suf = suffix[(Math.random()*suffix.length)|0];
-
+    const pre = prefix[(Math.random() * prefix.length) | 0];
+    const suf = suffix[(Math.random() * suffix.length) | 0];
+    const text = [pre, title, suf, link,'#自动发微博#'].join('');
     let options = {
       url: 'https://api.weibo.com/2/statuses/update.json',
       method: 'POST',
@@ -33,11 +33,13 @@ fs.readFile('./public/atom.xml', 'utf8', function (err, data) {
       },
       form: {
         source: cfg.weibo.appkey,
-        status: pre + title + suf + link
+        status: text
       }
     };
 
-    request(options);
+    request(options, function (err, response, body) {
+      console.log(body);
+    });
 
   })
 });
