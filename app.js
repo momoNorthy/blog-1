d4c91b75baed43d3a14b1955412e694cc64d3e62
Sabrina Luo <luohaojing@gmail.com>
2016-07-07 18:35:10 +0800

    update app weibo post msg

--- app.js
+++ app.js
@@ -9,17 +9,11 @@ fs.readFile('./public/atom.xml', 'utf8', function (err, data) {
   xml2js.parseString(data, function (err, data) {
     const post = data.feed.entry[0];
     const title = ' 《' + post.title[0] + '》 ';
-    const link = '（>>> ' + title + post.link[0]['$'].href + ' <<<）';
+    const link = title + post.link[0]['$'].href;
     const prefix = [
-      '集美貌与智慧于一身的@HiiTea 刚刚更新了博客(๑•̀ㅂ•́)و✧',
-      '勤劳又美丽的@HiiTea 又更新博客啦ヽ(✿ﾟ▽ﾟ)ノ',
-      '本宝宝@HiiTea 更新了博客╮(•́ω•̀)╭'
+      '发表了新的博客'
     ];
-    const suffix = [
-      '看看谁能抢到第一个沙发！',
-      '你们不理我，我会哭的哦QAQ',
-      '走过路过千万别错过！'
-    ];
+    const suffix = [];
 
     const pre = prefix[(Math.random() * prefix.length) | 0];
     const suf = suffix[(Math.random() * suffix.length) | 0];
